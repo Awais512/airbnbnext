39320eaa04b83ed9b7a2bee8b26b3918a529b8d3
awais512 <raza.awais25@gmail.com>
2023-04-13 05:18:07 +0500

    Get Current User

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,9 +1,16 @@
+import React from "react";
+import { User } from "@prisma/client";
 import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
 
-const Navbar = () => {
+interface NavbarProps {
+  currentUser?: User | null;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
+  console.log({ currentUser });
   return (
     <div className="fixed w-full shadow-sm bg-white z-10">
       <div className="py-4 border-b-[1px]">
