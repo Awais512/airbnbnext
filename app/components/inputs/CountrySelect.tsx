873b2c4a9a779c4db7b1b5f8cd33c555e5358cfe
awais512 <raza.awais25@gmail.com>
2023-04-26 23:29:28 +0500

    Adding country select box

---
+++ app/components/inputs/CountrySelect.tsx
@@ -0,0 +1,33 @@
+import useCountries from "@/app/hooks/useCountries";
+import React from "react";
+import Select from "react-select";
+
+export type CountrySelectValue = {
+  flag: string;
+  label: string;
+  latlng: number;
+  region: string;
+  value: string;
+};
+
+interface CountrySelectProps {
+  value?: CountrySelectValue;
+  onChange: (Value: CountrySelectValue) => void;
+}
+
+const CountrySelect: React.FC<CountrySelectProps> = ({ value, onChange }) => {
+  const { getAll } = useCountries();
+  return (
+    <div>
+      <Select
+        placeholder="Anywhere"
+        isClearable
+        options={getAll()}
+        value={value}
+        onChange={(value) => onChange(value as CountrySelectValue)}
+      />
+    </div>
+  );
+};
+
+export default CountrySelect;
