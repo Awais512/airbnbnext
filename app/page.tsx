5c7de48429eada0d22215dfe8b178f52d2eeff9b
awais512 <raza.awais25@gmail.com>
2023-05-04 20:47:47 +0100

    Listing Showing using server components

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <div className="text-rose-500 text-2xl">Hello Airbnb!</div>;
+import getListings from "./actions/getListings";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+
+export default async function Home() {
+  const listings = await getListings();
+  const isEmpty = true;
+
+  if (listings.length === 0) {
+    return <EmptyState showReset />;
+  }
+
+  return (
+    <Container>
+      <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        {listings.map((listing: any) => (
+          <h1 key={listing.id}>{listing.title}</h1>
+        ))}
+      </div>
+    </Container>
+  );
 }
