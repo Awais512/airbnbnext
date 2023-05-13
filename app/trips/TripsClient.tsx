3c5e0386806f38206b6500a9cf69cdcec31d2718
awais512 <raza.awais25@gmail.com>
2023-05-13 18:55:32 +0100

    Reservations cancel functionality

--- app/trips/TripsClient.tsx
+++ app/trips/TripsClient.tsx
@@ -45,6 +45,7 @@ const TripsClient: React.FC<TripsClientProps> = ({
         title="Trips"
         subtitle="Where you've benn and where you're going"
       />
+      <br />
       <div className="md-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
         {reservations.map((reservation) => (
           <ListingCard
