d7960743a48fbe3fb6f1fb7f2323234538329742
awais512 <raza.awais25@gmail.com>
2023-05-19 17:22:31 +0100

    Search Modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import ToasterProvider from "./providers/ToasterProvider";
 import LoginModal from "./components/modals/LoginModal";
 import getCurrentUser from "./actions/getCurrentUser";
 import RentModal from "./components/modals/RentModal";
+import SearchModal from "./components/modals/SearchModal";
 
 export const metadata = {
   title: "AirBnB",
@@ -27,6 +28,7 @@ export default async function RootLayout({
       <body className={font.className}>
         <ToasterProvider />
         <RentModal />
+        <SearchModal />
         <LoginModal />
         <RegisterModal />
         <Navbar currentUser={currentUser} />
